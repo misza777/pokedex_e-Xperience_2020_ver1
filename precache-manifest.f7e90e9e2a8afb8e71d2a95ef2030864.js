ecef253315334b39f99604278cf819f27f89642e
Michal Nowak <miau.nowak@gmail.com>
2020-04-14 15:48:47 +0200

    Updates

---
+++ precache-manifest.f7e90e9e2a8afb8e71d2a95ef2030864.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd0cb93a0fb017f04f2a4948f11ec084",
+    "url": "/pokedex_e-Xperience_2020_ver1/index.html"
+  },
+  {
+    "revision": "2e18dbaf57e74c3562d6",
+    "url": "/pokedex_e-Xperience_2020_ver1/static/css/main.3c25ad0b.chunk.css"
+  },
+  {
+    "revision": "836b78d837ba6afc331c",
+    "url": "/pokedex_e-Xperience_2020_ver1/static/js/2.d6683a43.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pokedex_e-Xperience_2020_ver1/static/js/2.d6683a43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e18dbaf57e74c3562d6",
+    "url": "/pokedex_e-Xperience_2020_ver1/static/js/main.ecc6e683.chunk.js"
+  },
+  {
+    "revision": "779080219fd31cbe8d84",
+    "url": "/pokedex_e-Xperience_2020_ver1/static/js/runtime-main.82915736.js"
+  }
+]);
